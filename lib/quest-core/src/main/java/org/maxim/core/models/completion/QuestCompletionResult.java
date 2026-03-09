4e95c6b35751344b53dda64cc136e256f68379d9
Maximun <rajatvermayt78645@gmail.com>
2026-03-09 17:36:19 +0530

    Cleanup after moving things around

--- lib/quest-core/src/main/java/org/maxim/core/models/completion/QuestCompletionResult.java
+++
@@ -1,5 +0,0 @@
-package org.maxim.core.models.completion;
-
-public class QuestCompletionResult {
-    public boolean completed = false;
-}
--- lib/quest-core/src/main/java/org/maxim/core/session/Session.java
+++
@@ -1,9 +0,0 @@
-package org.maxim.core.session;
-
-import org.maxim.core.models.response.JsonObject;
-
-public interface Session {
-    /* Interface for interacting with the api */ 
-    public JsonObject get(String path);
-    public JsonObject post(String path, Object body);
-}
